2a33de669a2d0b38aed0d8c01cce5e754e6526dd
Mesha Vegas <84249301+Meshavegas@users.noreply.github.com>
2023-09-02 20:02:05 +0100

    Update sanity,config.ts

--- dev/project_next13_car_showcase/sanity,config.ts
+++ dev/project_next13_car_showcase/sanity,config.ts
@@ -6,7 +6,7 @@ export const config = defineConfig({
   projectId: "2vylmok6",
   dataset: "production",
   title: "Fadhyl store Admin",
-  apiVersion: "2023-09-1",
+  apiVersion: "2023-03-04",
   basePath: "/admin",
   plugins: [deskTool()],
   schema: { types: schemas },
