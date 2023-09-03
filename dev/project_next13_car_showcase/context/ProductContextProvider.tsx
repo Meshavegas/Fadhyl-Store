8aacc22870abea2f5252b72d21d8abf53838eca2
Mesha Vegas <84249301+Meshavegas@users.noreply.github.com>
2023-09-03 17:49:03 +0100

    Create ProductContextProvider.tsx

---
+++ dev/project_next13_car_showcase/context/ProductContextProvider.tsx
@@ -0,0 +1,25 @@
+import { Product } from "@types/modele/product";
+import React, { ReactNode, useState } from "react";
+import { ProductContext } from "./productContext";
+interface IProps {
+  children: ReactNode;
+}
+
+const ProductContextProvider = ({ children }: IProps) => {
+  const [products, setProduct] = useState<Product[]>([]);
+  const addProduct = (product: Product) => {
+    setProduct([...products, product]);
+  };
+  return (
+    <ProductContext.Provider
+      value={{
+        products,
+        addProduct,
+      }}
+    >
+      {children}
+    </ProductContext.Provider>
+  );
+};
+
+export default ProductContextProvider;
