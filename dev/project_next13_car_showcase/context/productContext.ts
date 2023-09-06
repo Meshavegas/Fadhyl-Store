b52afbde21129c50eb3634d728aee28e1985c012
Mesha Vegas <84249301+Meshavegas@users.noreply.github.com>
2023-09-06 18:53:23 +0100

    Update productContext.ts

--- dev/project_next13_car_showcase/context/productContext.ts
+++ dev/project_next13_car_showcase/context/productContext.ts
@@ -1,14 +1,17 @@
+import { ProductInOrder } from "../types/modele/commande";
 import { Product } from "../types/modele/product";
 import { createContext, useContext } from "react";
 
 interface IProductContext {
-  products: Product[];
+  products: ProductInOrder[];
   addProduct: (product: Product) => void;
+  addOneProduct: (product: ProductInOrder[]) => void;
 }
 
 export const ProductContext = createContext<IProductContext>({
   products: [],
   addProduct(product) {},
+  addOneProduct(product) {},
 });
 
 export const useProductContext = () => useContext(ProductContext);
