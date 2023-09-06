2d261a5c0cd09ff1b03815b688d4f98ab51c8d83
Mesha Vegas <84249301+Meshavegas@users.noreply.github.com>
2023-09-06 18:53:40 +0100

    Create usercontextProvider.tsx

---
+++ dev/project_next13_car_showcase/context/user/usercontextProvider.tsx
@@ -0,0 +1,28 @@
+"use client";
+import React, { ReactNode, useState } from "react";
+
+import { User } from "../../types/modele/user";
+import { UserContext } from "./userContext";
+interface IProps {
+  children: ReactNode;
+}
+
+const UserContextProvider = ({ children }: IProps) => {
+  const [user, setProduct] = useState<User | null>(null);
+
+  const login = (p: User) => {
+    setProduct(p);
+  };
+  return (
+    <UserContext.Provider
+      value={{
+        user,
+        login,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserContextProvider;
