c3dfa2de28efa164f193b16e2fe96f68d77f2c50
Mesha Vegas <84249301+Meshavegas@users.noreply.github.com>
2023-09-29 09:21:23 +0100

    Update client.ts

--- dev/project_next13_car_showcase/sanity/config/client.ts
+++ dev/project_next13_car_showcase/sanity/config/client.ts
@@ -5,7 +5,6 @@ const client = {
   dataset: "production",
   apiVersion: "2023-09-03",
   useCdn: true,
-  token: process.env.NEXT_PUBLIC_TOKEN,
   ignoreBrowserTokenWarning: true,
 };
 const configC = createClient(client);
